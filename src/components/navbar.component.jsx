deb7226069031784711ca8cb7fff255a622ce417
Animan Amit <animanamit@wustl.edu>
2021-11-13 17:24:27 +0530

    first commit

---
+++ src/components/navbar.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <Link to="/">Navbar</Link>
+    </div>
+  );
+};
+
+export default Navbar;
